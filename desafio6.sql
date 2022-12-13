672688827d26f488e95de66803864aa2d119e8b1
Marcel Albuquerque <msalbuquerque@hotmail.com>
2022-12-13 17:51:25 -0300

    feat: req-06 - exibindo relatorio de faturamento.

---
+++ desafio6.sql
@@ -0,0 +1,9 @@
+SELECT 
+	MIN(p.valor) AS faturamento_minimo,
+  MAX(p.valor) AS faturamento_maximo,
+  ROUND(AVG(p.valor), 2) AS faturamento_medio,
+	SUM(p.valor) AS faturamento_total
+FROM
+  plano AS p
+INNER JOIN
+	usuario AS u ON p.plano_id = u.plano_id;
